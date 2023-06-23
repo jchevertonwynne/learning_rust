7f2e1148f36cf0ea798606ddf0eb6957cab2fa4d
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2023-06-23 10:50:07 +0100

    made redis request type wrapper for better introspection

--- course/part07/tracing_showcase/src/bin/redis_stuff.rs
+++ course/part07/tracing_showcase/src/bin/redis_stuff.rs
@@ -1,7 +1,7 @@
 use std::{pin::Pin, sync::Arc};
 
 use futures::Future;
-use redis::aio::ConnectionLike;
+use redis::{aio::ConnectionLike, ToRedisArgs};
 use tokio::sync::Mutex;
 use tower::{Service, ServiceBuilder};
 use tracing::{info, info_span, Instrument};
@@ -22,22 +22,22 @@ async fn main() -> anyhow::Result<()> {
     let r = redis::Client::open("redis://127.0.0.1:6379")?;
     let conn = Arc::new(Mutex::new(
         r.get_tokio_connection()
-            .instrument(info_span!("connecting to redis"))
+            .instrument(info_span!("getting async redis connection"))
             .await?,
     ));
 
     let mut service = ServiceBuilder::new()
-        .layer(RequestCounterLayer::new(RedisChecker::new(
+        .layer(RequestCounterLayer::new(RedisGetChecker::new(
             redis::Value::Data("world2".as_bytes().to_vec()),
         )))
         .service(RedisService { conn });
 
-    service.call(redis::Cmd::set("hello", "world")).await?;
-    let resp = service.call(redis::Cmd::get("hello")).await?;
+    service.call(RedisRequest::set("hello", "world")).await?;
+    let resp = service.call(RedisRequest::get("hello")).await?;
     info!(" got resp {resp:?}");
 
-    service.call(redis::Cmd::set("hello", "world2")).await?;
-    let resp = service.call(redis::Cmd::get("hello")).await?;
+    service.call(RedisRequest::set("hello", "world2")).await?;
+    let resp = service.call(RedisRequest::get("hello")).await?;
     info!(" got resp {resp:?}");
 
     entered.exit();
@@ -47,23 +47,50 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+struct RedisRequest {
+    cmd: redis::Cmd,
+    request_type: RedisRequestType,
+}
+
+impl RedisRequest {
+    fn get<K: ToRedisArgs>(key: K) -> Self {
+        RedisRequest {
+            cmd: redis::Cmd::get(key),
+            request_type: RedisRequestType::Get,
+        }
+    }
+
+    fn set<K: ToRedisArgs, V: ToRedisArgs>(key: K, val: V) -> Self {
+        RedisRequest {
+            cmd: redis::Cmd::set(key, val),
+            request_type: RedisRequestType::Set,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+enum RedisRequestType {
+    Set,
+    Get,
+}
+
 #[derive(Debug, Clone)]
-struct RedisChecker {
+struct RedisGetChecker {
     wanted: redis::Value,
 }
 
-impl RedisChecker {
+impl RedisGetChecker {
     fn new(wanted: redis::Value) -> Self {
         Self { wanted }
     }
 }
 
-impl SuccessChecker for RedisChecker {
-    type Request = redis::Cmd;
+impl SuccessChecker for RedisGetChecker {
+    type Request = RedisRequest;
     type Response = redis::Value;
 
     fn should_monitor_response(&self, _req: &Self::Request) -> bool {
-        true
+        matches!(_req.request_type, RedisRequestType::Get)
     }
 
     fn is_successful_response(&self, res: &Self::Response) -> bool {
@@ -75,7 +102,7 @@ struct RedisService {
     conn: Arc<Mutex<redis::aio::Connection>>,
 }
 
-impl Service<redis::Cmd> for RedisService {
+impl Service<RedisRequest> for RedisService {
     type Response = redis::Value;
 
     type Error = redis::RedisError;
@@ -89,11 +116,12 @@ impl Service<redis::Cmd> for RedisService {
         std::task::Poll::Ready(Ok(()))
     }
 
-    fn call(&mut self, req: redis::Cmd) -> Self::Future {
+    fn call(&mut self, req: RedisRequest) -> Self::Future {
+        let RedisRequest { cmd, request_type } = req;
         let conn = self.conn.clone();
         Box::pin(
-            async move { conn.lock().await.req_packed_command(&req).await }
-                .instrument(info_span!("making a redis request")),
+            async move { conn.lock().await.req_packed_command(&cmd).await }
+                .instrument(info_span!("making a redis request", request_type = ?request_type)),
         )
     }
 }
